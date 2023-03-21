e7fb8df439ff5a27dc3a72bc23a3a819a6311de4
jyoti <jyotikumari123>
2023-03-21 16:40:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+const colorPickerBtn= document.getElementById("color-picker").addEventListener("click", () => {
+    const resultElement = document.getElementById("result");
+  
+    if (!window.EyeDropper) {
+      resultElement.textContent =
+        "Your browser does not support the EyeDropper API";
+      return;
+    }
+  
+    const eyeDropper = new EyeDropper();
+  
+    eyeDropper
+      .open()
+      .then((result) => {
+        resultElement.textContent = result.sRGBHex;
+        resultElement.style.backgroundColor = result.sRGBHex;
+      })
+      .catch((e) => {
+        resultElement.textContent = e;
+      });
+  });
+  
+
+
